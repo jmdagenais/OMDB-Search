13bb35e78b2562c2ca32a9cd9a76f810cd447c42
jmdagenais <jmdagenais@acquisio.com>
2017-06-09 11:08:17 -0400

    read the api key from a json file

--- src/app/shared/movie.service.ts
+++ src/app/shared/movie.service.ts
@@ -5,10 +5,22 @@ import {Http, Response} from '@angular/http';
 
 @Injectable()
 export class MovieService {
-  constructor(private http: Http) {  }
+  apiKey: string;
+
+  constructor(private http: Http) {
+    this.getApiKey();
+  }
+
+  private getApiKey() {
+    this.http.get('assets/config.json')
+      .map((res: Response) => res.json())
+      .subscribe((value: any) => {
+        this.apiKey = value.apikey;
+      });
+  }
 
   searchMovies(query: string): Observable<Array<Movie>> {
-    return this.http.get(`http://www.omdbapi.com/?s=${query}`)
+    return this.http.get(`http://www.omdbapi.com/?s=${query}&apikey=${this.apiKey}`)
       .map((res: Response) => res.json())
       .map((value: any) => {
           if (value.Response === 'True') {
@@ -28,7 +40,7 @@ export class MovieService {
   }
 
   getMovie(id: string): Observable<MovieDetails> {
-    return this.http.get(`http://www.omdbapi.com/?i=${id}`)
+    return this.http.get(`http://www.omdbapi.com/?i=${id}&apikey=${this.apiKey}`)
       .map((res: Response) => res.json());
   }
 }
