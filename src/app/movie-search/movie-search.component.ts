390bb6f18ece2e385b73dc24c34f41a55bf822ee
jmdagenais <jmdagenais@acquisio.com>
2017-06-26 14:44:08 -0400

    utilisation de debounce pour limiter le nombre d'appel à l'api

--- src/app/movie-search/movie-search.component.ts
+++ src/app/movie-search/movie-search.component.ts
@@ -1,27 +1,46 @@
 import 'rxjs/Rx';
-import {Component} from '@angular/core';
+import {Component, OnInit, ViewChild, ElementRef, OnDestroy} from '@angular/core';
 import {Movie} from '../shared/movie.interface';
 import {MovieService} from '../shared/movie.service';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-movie-search',
   templateUrl: './movie-search.component.html',
   styleUrls: ['./movie-search.component.css']
 })
-export class MovieSearchComponent {
+export class MovieSearchComponent implements OnInit, OnDestroy {
   movies: Array<Movie>;
+  @ViewChild('searchField') searchField: ElementRef;
+
+  inputSubscription: Subscription;
+  movieSubscrption: Subscription;
 
   constructor(private movieService: MovieService) { }
 
-  searchMovie(ev) {
-    let query: string = ev.target.value;
+  ngOnInit() {
+    this.inputSubscription =  Observable.fromEvent(this.searchField.nativeElement, 'keyup')
+      .map((ev: any) => ev.target.value)
+      .debounceTime(500)
+      .subscribe((value: string) => {
+        this.searchMovie(value);
+      });
+  }
+
+  searchMovie(query) {
     if (query.length === 0) {
       this.movies = [];
       return;
     }
-    this.movieService.searchMovies(query)
+    this.movieSubscrption = this.movieService.searchMovies(query)
       .subscribe((movies: Array<Movie>) => {
         this.movies = movies;
       });
+  }
+
+  ngOnDestroy() {
+    this.inputSubscription.unsubscribe();
+    this.movieSubscrption.unsubscribe();
   }
 }
